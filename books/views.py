275ea50a67232fc4fbb4dc04ab8cae18ec7526a3
Izzet-Agayev <agayevizzet56@gmail.com>
2025-01-11 11:04:29 +0400

    Create book templates and views

--- books/views.py
+++ books/views.py
@@ -29,15 +29,15 @@ class CreateBookView(View):
         return render(request, 'books/create_book.html', context)
     
     def post(self, request):
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
+        book_form = BookForm(request.POST)
+        if book_form.is_valid():
+            book_form.save()
 
             messages.success(request, 'Kitab elanı uğurla yaradıldı')
             return redirect('create_book')
         
         else:
-            messages.error(request, form.errors)
+            messages.error(request, book_form.errors)
             return redirect('create_book')
 
 
@@ -55,3 +55,60 @@ class DetailBookView(View):
         }
 
         return render(request, 'books/detail_book.html', context)
+    
+    def post(self, request, pk):
+        book = self.get_object(pk=pk)
+
+        context = {
+            'book': book
+        }
+
+        return render(request, 'books/detail_book.html', context)
+
+
+class UpdateBookView(View):
+    def get_object(self, pk):
+        books = Book.objects.select_related('categories')
+        book = get_object_or_404(books, pk=pk)
+        return book
+    
+    def get(self, request, pk):
+        book = self.get_object(pk=pk)
+        book_form = BookForm(instance=book)
+        context = {
+            'book_form': book_form
+        }
+
+        return render(request, 'books/update_book.html', context)
+    
+    def post(self, request, pk):
+        book = self.get_object(pk=pk)
+        book_form = BookForm(request.POST, instance=book)
+        if book_form.is_valid():
+            book_form.save()
+            return redirect('detail_book', book.id)
+        else:
+            messages.error(request, book_form.errors)
+            return redirect('update_book', book.id)
+        
+
+class DeleteBookView(View):
+    def get_object(self, pk):
+        books = Book.objects.select_related('categories')
+        book = get_object_or_404(books, pk=pk)
+        return book
+
+    def get(self, request, pk):
+        book = self.get_object(pk=pk)
+        self.perform_delete(obj=book)
+        messages.success(request, f'{book.title} adlı kitab elanı silindi.')
+        return redirect('all_books')
+    
+    def post(self, request, pk):
+        book = self.get_object(pk=pk)
+        self.perform_delete(obj=book)
+        messages.success(request, f'{book.title} adlı kitab elanı silindi.')
+        return redirect('all_books')
+
+    def perform_delete(self, obj):
+        obj.delete()
